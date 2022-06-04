76bcf0e6e38d19bd68a8576c09ddc4e8e0c8a952
ArtemLeshch <artemleshchukov02@gmail.com>
2022-06-04 09:44:17 +0400

    added loading machine code into executable memory, organized functions

--- VirtualMachine.c
+++ VirtualMachine.c
@@ -2,10 +2,28 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+/* MEMORY MAPPED REGISTERS */
+enum
+{
+	MR_KBSR = 0xFE00;	/* keyboard status */
+	MR_KBDR = 0xFE02;	/* keyboard data */	
+};
+/* TRAP CODES */
+enum 
+{
+	TRAP_GETC = 0x20, 	/* get character from keyboard, not echoed onto the terminal */
+	TRAP_OUT = 0x21,	/* output a character */
+	TRAP_PUTS = 0x22, 	/* output a word string */
+	TRAP_IN = 0x23,		/* get character from keyboar, echoed onto the terminal */
+	TRAP_PUTSP = 0x24,	/* output a byte string */
+	TRAP_HALT = 0x25,	/* halt the program */
+};
 
+/* MEMORY STORAGE */
 #define MEMORY_MAX (1 << 16)
 uint16_t memory[MEMORY_MAX];	/* 65536 locations */
 
+/* REGISTERS and REGISTER STORAGE*/
 enum 
 {
 	R_R0 = 0,
@@ -22,6 +40,7 @@ enum
 };
 uint16_t reg[R_COUNT];
 
+/* OPCODES */
 enum 
 {
 	OP_BR = 0,	// 	branch
@@ -41,7 +60,7 @@ enum
 	OP_LEA,		//	load effective address
 	OP_TRAP		//	execute trap
 };
-
+/* FLAGS */
 enum 
 {
 	FL_POS = 1 << 0; 	/* P */
@@ -49,22 +68,93 @@ enum
 	FL_NEG = 1 << 2;	/* N */
 };
 
-/* TRAP CODES */
-enum 
+/* SIGN EXTENSION */
+uint16_t sign_extend(uint16_t x, int bit_count) 
 {
-	TRAP_GETC = 0x20, 	/* get character from keyboard, not echoed onto the terminal */
-	TRAP_OUT = 0x21,	/* output a character */
-	TRAP_PUTS = 0x22, 	/* output a word string */
-	TRAP_IN = 0x23,		/* get character from keyboar, echoed onto the terminal */
-	TRAP_PUTSP = 0x24,	/* output a byte string */
-	TRAP_HALT = 0x25,	/* halt the program */
-};
+	if ((x >> (bit_count - 1)) & 1) 
+	{
+		x |= (0xFFFF << bit_count);
+	}
+	return x;
+}
 
+/* SWAP LITTLE-ENDIAN TO BIG-ENDIAN */
+uint16_t swap16(uint16_t x)
+{
+	return (x << 8) | (x >> 8);
+}
 
-uint16_t sign_extend(uint16_t x, int bit_count);
-void update_flags(uint16_t r);
+void update_flags(uint16_t r) 
+{
+	if (reg[r] == 0)
+	{
+		reg[R_COND] = FL_ZRO;
+	}
+	else if (reg[r] >> 15)		/* a 1 in the left-most bit indicates negative */
+	{
+		reg[R_COND] = FL_NEG;	
+	}
+	else 
+	{
+		reg[R_COND] = FL_POS;
+	}
+}
 
+void read_image_file(FILE* file)
+{
+	/* the origin tells us where in memory to place the image */
+	uint16_t origin;
+	fread(&origin, sizeof(origin), 1, file);
+	origin = swap16(origin);
+	
+	/* we know the maximum file size so we only need one fread */
+	uint16_t max_read = MEMORY_MAX - origin;
+	uint16_t* p = memory + origin;
+	size_t read = fread(p, sizeof(uint16_t), max_read, file);	//	maybe make it read until EOF 
+	
+	while (read-- > 0)
+	{
+		*p = swap16(*p);
+		++p;
+	}
+}
 
+int read_image(const char* image_path)
+{
+	FILE* file = fopen(image_path, "rb");
+	if (!file) { return 0; }
+	read_image_file(file);
+	fclose(file);
+	return 1;
+}
+
+/* CHECK KEY */
+
+/* MEMORY ACCESS */
+void mem_write(uint16_t address, uint16_t val)
+{
+	memory[address] = val;
+}
+uint16_t mem_read(uint16_t address)
+{	
+	if (address == MR_KBSR)
+	{
+		if (check_key())
+		{
+			memory[MR_KBSR] = (1 << 15);
+			memory[MR_KBDR] = getchar();
+		}
+		else 
+		{
+			memory[MR_KBSR] = 0;
+		}
+	}
+	return memory[address];
+}
+
+/* INPUT BUFFERING AND HANDLE INTERRUPT */
+
+/* MAIN LOOP */
 int main(int argc, const char* argv[])
 {
 	/* setup */
@@ -349,32 +439,3 @@ int main(int argc, const char* argv[])
 	
 	return 0;
 }
-
-uint16_t sign_extend(uint16_t x, int bit_count) 
-{
-	if ((x >> (bit_count - 1)) & 1) 
-	{
-		x |= (0xFFFF << bit_count);
-	}
-	return x;
-}
-
-void update_flags(uint16_t r) 
-{
-	if (reg[r] == 0)
-	{
-		reg[R_COND] = FL_ZRO;
-	}
-	else if (reg[r] >> 15)		/* a 1 in the left-most bit indicates negative */
-	{
-		reg[R_COND] = FL_NEG;	
-	}
-	else 
-	{
-		reg[R_COND] = FL_POS;
-	}
-}
-
-
-
-
